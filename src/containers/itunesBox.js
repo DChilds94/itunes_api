68725a0468a1c0b167b6f4f2444f667f806bb58c
Daniel Childs <dc.1994@outlook.com>
2018-06-26 16:45:05 +0100

    render test for itunesbox.js

---
+++ src/containers/itunesBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ItunesBox extends React.Component {
+
+
+  render(){
+    return (
+      <div>
+        <p>test</p>
+      </div>
+    )
+  }
+}
+
+export default ItunesBox
